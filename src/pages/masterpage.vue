58a438ef4f6c9f59b214f2ffb4cf0d9a947d0cad
joueur <lxp775399418@163.com>
2024-12-18 21:46:07 +0800

    ddd

--- src/pages/masterpage.vue
+++ src/pages/masterpage.vue
@@ -3,6 +3,7 @@
    
     <el-container>
         
+        
         <el-aside width="200px"  style="background-color: rgb(238, 241, 246)">
             <el-menu :default-openeds="['1', '3']">
                 <el-submenu index="1">
